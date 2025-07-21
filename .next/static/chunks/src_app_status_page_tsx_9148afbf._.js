6b135b93def5ee0f89c3da287d6afff4ca1ff68c
koson123 <136363307+koson123@users.noreply.github.com>
2025-07-21 14:57:54 -0600

    Add .next/static/chunks/src_app_status_page_tsx_9148afbf._.js 1753131474209

---
+++ .next/static/chunks/src_app_status_page_tsx_9148afbf._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_2c980970._.js",
+  "static/chunks/src_f69e1982._.js"
+],
+    source: "dynamic"
+});
